461764eda76305d44cc076c05441a029f0bc1d71
Alex Guevara <abg28@duke.edu>
2018-09-23 18:52:40 -0400

    adds test file

---
+++ test_fun.py
@@ -0,0 +1,8 @@
+def test_add_three():
+    from fun import add_three
+    assert(add_three(2, 3, 4) == 9)
+
+
+def test_divide_by_three():
+    from fun import divide_by_three
+    assert(divide_by_three(6) == 2)
